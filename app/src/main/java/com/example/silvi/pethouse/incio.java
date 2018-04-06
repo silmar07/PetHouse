4b93d248355203b17fbfe0a1fcd0a39a59e34981
jenniza <jezapata@ucm.es>
2018-04-06 13:21:25 +0200

    hola

--- app/src/main/java/com/example/silvi/pethouse/incio.java
+++ app/src/main/java/com/example/silvi/pethouse/incio.java
@@ -9,7 +9,7 @@ public class incio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incio);
-        //pruebaç
+        //pruebas
         
 
     /*hola*/
