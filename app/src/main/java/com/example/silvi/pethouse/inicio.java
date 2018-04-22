cf749fdedc1faf706790a116ca13d6b533da6dc4
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-04-22 12:36:52 +0200

    Cambio menor

--- app/src/main/java/com/example/silvi/pethouse/inicio.java
+++ app/src/main/java/com/example/silvi/pethouse/inicio.java
@@ -5,14 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class inicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_incio);
+        setContentView(R.layout.activity_inicio);
 
         Button button = findViewById(R.id.btnReg);
 
