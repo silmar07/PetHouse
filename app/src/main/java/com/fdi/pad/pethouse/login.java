4a75551b8942f6307f64abe34b838f04003a91ba
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-05-03 17:43:34 +0200

    Se añade conexión Firebase

--- app/src/main/java/com/fdi/pad/pethouse/login.java
+++ app/src/main/java/com/fdi/pad/pethouse/login.java
@@ -1,31 +1,47 @@
 package com.fdi.pad.pethouse;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
-import android.os.AsyncTask;
+
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.fdi.pad.pethouse.database.DatabaseUser;
-import com.fdi.pad.pethouse.entities.User;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity implements View.OnClickListener {
 
-    private Button buttonRegister, buttonEnter;
-    private EditText editTextEmail, editTextPassword;
+    private static final String TAG = "PetHouse";
+
+    private Button buttonRegister;
+    private Button buttonEnter;
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         buttonRegister = (Button) findViewById(R.id.register_button);
         buttonRegister.setOnClickListener(this);
-
         buttonEnter = (Button) findViewById(R.id.enter_button);
         buttonEnter.setOnClickListener(this);
 
@@ -42,7 +58,17 @@ public class login extends AppCompatActivity implements View.OnClickListener {
             case R.id.enter_button:
                 EnterButton();
                 break;
-        }//Switch
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            updateUI(currentUser);
+        }
     }
 
     public void RegisterButton() {
@@ -51,41 +77,55 @@ public class login extends AppCompatActivity implements View.OnClickListener {
     }
 
     public void EnterButton() {
+        if (!validateForm()) {
+            return;
+        }
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
 
-        if (!email.equals("") || password.equals("")) {
-            new DatabaseLogin().execute(email, password);
-        } else {
-            Toast.makeText(getApplicationContext(), "Debes rellenar todos los campos", Toast.LENGTH_SHORT).show();
-        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
-    private class DatabaseLogin extends AsyncTask<String, Void, User> {
-
-        private ProgressDialog dialog = new ProgressDialog(login.this);
-
-        @Override
-        protected void onPreExecute() {
-            this.dialog.setMessage("Entrando, espera");
-            this.dialog.show();
-        }
-
-        @Override
-        protected User doInBackground(String... strings) {
-            return DatabaseUser.checkUser(strings[0], strings[1]);
-        }
-
-        @Override
-        protected void onPostExecute(User result) {
-            this.dialog.dismiss();
-            processLogin(result);
-        }
-    }
-    private void processLogin(User user) {
-
+    private void updateUI(FirebaseUser currentUser) {
         Intent intent = new Intent(login.this, home.class);
         startActivity(intent);
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = editTextEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            editTextEmail.setError("Required.");
+            valid = false;
+        } else {
+            editTextEmail.setError(null);
+        }
+
+        String password = editTextPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Required.");
+            valid = false;
+        } else {
+            editTextPassword.setError(null);
+        }
+        return valid;
     }
 }
 
