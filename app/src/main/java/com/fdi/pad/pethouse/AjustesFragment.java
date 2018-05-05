56a8f612a8549e2f481f38c94ea3654bb625f4fe
silvia <silmar07@ucm.es>
2018-05-05 21:31:52 +0200

    vistas
    vista de home y ajustes

--- app/src/main/java/com/fdi/pad/pethouse/AjustesFragment.java
+++ app/src/main/java/com/fdi/pad/pethouse/AjustesFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.content.Intent;
 
 /**
  * Created by silvi on 4/5/18.
@@ -13,10 +15,39 @@ import android.view.ViewGroup;
 
 public class AjustesFragment extends Fragment {
 
+    private Button infor, cerrar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ajustes,null);
     }
+
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+
+
+        //eventos de los textView
+        infor = (Button) getView().findViewById(R.id.btnInfor);
+        cerrar = (Button) getView().findViewById(R.id.btnCerrarSesion);
+
+        infor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(),InforApp.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/fdi/pad/pethouse/HomeFragment.java
+++ app/src/main/java/com/fdi/pad/pethouse/HomeFragment.java
@@ -1,11 +1,18 @@
 package com.fdi.pad.pethouse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.net.Uri;
+
+import java.util.ArrayList;
 
 /**
  * Created by silvi on 4/5/18.
@@ -13,10 +20,69 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayList<AnuncioList> listaAnuncios;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,null);
     }
+
+
+    @Override
+    public void onActivityCreated(Bundle state) {
+        super.onActivityCreated(state);
+
+        listView = (ListView)getView().findViewById(R.id.listaAnuncios);
+
+        cargarLista();
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                AnuncioList anuncio = AnuncioList.getAnuncio(position,listaAnuncios);
+                Uri s = anuncio.getUri();
+
+                Intent intent = new Intent(Intent.ACTION_VIEW,s);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void cargarLista() {
+
+        //creamso la lista dodne vamso a poner los enlaces
+        ArrayList<AnuncioList> lista = new ArrayList<>();
+
+        //creamos la lista de anuncios
+        cargarAnuncios();
+
+        int i = 0;
+        while( i < listaAnuncios.size()){
+            lista.add(listaAnuncios.get(i));
+            i++;
+        }
+
+        ArrayAdapter<AnuncioList> adapter = new ArrayAdapter<>(getActivity().getApplicationContext(),
+                android.R.layout.simple_expandable_list_item_1,lista);
+
+        listView.setAdapter(adapter);
+
+    }
+
+    private void cargarAnuncios() {
+
+        listaAnuncios = new ArrayList<>();
+
+        listaAnuncios.add(new AnuncioList("Anuncio 1","https://www.google.es/?gws_rd=ssl",0));
+        listaAnuncios.add(new AnuncioList("Anuncio 2","http://www.ucm.es",1));
+        listaAnuncios.add(new AnuncioList("Anuncio 3","https://www.youtube.com/?hl=es&gl=ES",2));
+        listaAnuncios.add(new AnuncioList("Anuncio 4","https://github.com",3));
+    }
+
 }
