fda395cb87825824ddab8358392fcb47b5dda527
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-05-07 09:30:21 +0200

    Se coge datos de la base de datos

--- app/src/main/java/com/fdi/pad/pethouse/activity_home.java
+++ app/src/main/java/com/fdi/pad/pethouse/activity_home.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.fdi.pad.pethouse.home.fragment_home_user;
+
 public class activity_home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private TextView mTextMessage;
@@ -46,7 +48,7 @@ public class activity_home extends AppCompatActivity implements BottomNavigation
             break;
 
             case R.id.btnUsuario:
-                fragment = new UsuarioFragment();
+                fragment = new fragment_home_user();
             break;
 
             case R.id.btnAjustes:
