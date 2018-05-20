be9f2b2943647a091237bf1baf9705d841f4dc61
silvia <silmar07@ucm.es>
2018-05-21 00:14:25 +0200

    varios cambios
    vistas con datos de la bbdd (lista mascota,lista anuncios)
    vista de crear anuncios y mascotas
    modificar mascota

---
+++ app/src/main/java/com/fdi/pad/pethouse/entities/Pet.kt
@@ -0,0 +1,49 @@
+package com.fdi.pad.pethouse.entities
+
+import android.os.Parcel
+import android.os.Parcelable
+
+/**
+ * Clase que representa un usuario de la aplicación.
+ */
+data class Pet(var name: String? = null,
+               var species: String? = null,
+               var breed: String? = null, //raza
+               var age: String? = null,
+               var stelilization: String? = null,
+               var medicalData: String? = null,
+               var otherData: String? = null) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(species)
+        parcel.writeString(breed)
+        parcel.writeString(age)
+        parcel.writeString(stelilization)
+        parcel.writeString(medicalData)
+        parcel.writeString(otherData)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Pet> {
+        override fun createFromParcel(parcel: Parcel): Pet {
+            return Pet(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Pet?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
